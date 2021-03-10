0133f85d5cd53a48504b01acc744d6396cbfba26
elvric <elvric.gtombert@gmail.com>
2021-03-10 17:00:59 +0100

    Removed Print statements

--- project1/smc_party.py
+++ project1/smc_party.py
@@ -103,7 +103,6 @@ class SMCParty:
                 a = self.process_expression(a)
                 b = self.process_expression(b)
                 if a.is_secret_share() and b.is_secret_share():
-                    print("Here")
                     # Beaver triplets Algorithm
                     # u = a, v = b, w = c and a = x, b = y
                     u, v, w = self.comm.retrieve_beaver_triplet_shares(str(expr.id.__hash__()))
@@ -116,13 +115,11 @@ class SMCParty:
                     y = b - v
                     self.comm.publish_message('beaver:x-a_' + str(expr.id.__hash__()), pickle.dumps(x))
                     self.comm.publish_message('beaver:y-b_' + str(expr.id.__hash__()), pickle.dumps(y))
-                    print("Here")
                     # reconstruct locally x - a and y - b (where x = a, a = u, y = b, b = v)
                     for client_id in self.protocol_spec.participant_ids:
                         if self.client_id != client_id:
                             x = x + pickle.loads(self.comm.retrieve_public_message(client_id, 'beaver:x-a_' + str(expr.id.__hash__())))
                             y = y + pickle.loads(self.comm.retrieve_public_message(client_id, 'beaver:y-b_' + str(expr.id.__hash__())))
-                    print("Here")
                     res = w + (a * y) + (b * x)
                     if self.is_additioner_client():
                         res = res - (x * y)
