769e099bcb79f43a33afc1379baef3537ffe7d55
elvric <elvric.gtombert@gmail.com>
2021-04-30 23:50:31 +0200

    generated traces using tor properly

--- project2/finger_printing/generate_run.py
+++ project2/finger_printing/generate_run.py
@@ -4,6 +4,6 @@ f = open('run_commands.txt', 'w+')
 for i in range(1, 4):
     for s in itertools.combinations(values, i):
         joined = ' '.join(s)
-        v = f" python3 client.py grid @ {joined}\n"
+        v = f" python3 client.py grid @ {joined} -t\n"
         f.write(v)
 f.close()
--- project2/generate_traces.sh
+++ project2/generate_traces.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-i=3
+i=0
 
-while [[ i=$((i+1)) -lt 5 ]]; do
+while [[ i=$((i+1)) -lt 101 ]]; do
   file_name="./finger_printing/network_capture_cell_$i.pcap"
   tshark -w $file_name &
   while read -r line; do
     cmd=${line//@/$i}
-    $cmd &> /dev/null
+    $cmd
+    echo "$cmd"
   done <./finger_printing/run_commands.txt
   echo 'done'
-  kill -2 %"$1"
+  kill -2 `jobs -p`
   chmod 777 $file_name
 done
