0f16c76b835bf6c661fb53d3a027fc9e26be0e78
elvric <elvric.gtombert@gmail.com>
2021-04-30 17:32:35 +0200

    part_4_trace

---
+++ project2/finger_printing/generate_run.py
@@ -0,0 +1,9 @@
+import itertools
+values = ['-T restaurant', '-T bar', '-T dojo']
+f = open('run_commands.txt', 'w+')
+for i in range(1, 4):
+    for s in itertools.combinations(values, i):
+        joined = ' '.join(s)
+        v = f" python3 client.py grid @ {joined}\n"
+        f.write(v)
+f.close()
