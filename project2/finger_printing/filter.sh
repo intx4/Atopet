cd4af1394f11df38932f1fc3418bde4862a70b01
elvric <elvric.gtombert@gmail.com>
2021-05-01 22:15:19 +0200

    filtered_version_available

--- project2/finger_printing/filter.sh
+++ project2/finger_printing/filter.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 filter='tls.app_data && frame.len >= 615'
-#ls ./raw | xargs -I @ tshark -r ./raw/@ -w filtered/@ -Y "$filter"
-tshark -r raw/network_capture_cell_1.pcap -w filtered/network_capture_cell_1.pcap -Y "$filter"
-echo 'done'
+ls -r raw | egrep ^cell_ > tmp.txt
+while read -r line; do
+  dir_name_new="./filtered/$line/"
+  mkdir "$dir_name_new" 2> /dev/null
+  dir_name_old="./raw/$line/"
+  ls "$dir_name_old" | xargs -I @ tshark -r "$dir_name_old"@ -Y "$filter" -w "$dir_name_new"@
+done <./tmp.txt
+rm tmp.txt
