f93806b37f0f1fe1713d1f1ab437c67ba01170e2
elvric <elvric.gtombert@gmail.com>
2021-05-01 10:38:29 +0200

    actual tor communication trace

--- project2/generate_traces.sh
+++ project2/generate_traces.sh
@@ -3,7 +3,7 @@ i=0
 
 while [[ i=$((i+1)) -lt 101 ]]; do
   file_name="./finger_printing/network_capture_cell_$i.pcap"
-  tshark -w $file_name &
+  tshark -i eth0 -w $file_name &
   while read -r line; do
     cmd=${line//@/$i}
     $cmd
