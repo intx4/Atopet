a3ddcdc08bbe4878d30f04f6cae073e543dbc748
elvric <elvric.gtombert@gmail.com>
2021-05-08 13:50:10 +0200

    cell 1 + helper scrpts

--- project2/generate_traces.sh
+++ project2/generate_traces.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 i=0
 while [[ i=$((i+1)) -lt 101 ]]; do
-  it=1
+  it=8
   while read -r line; do
     file_name="./finger_printing/raw/cell_$i/network_capture_round_$it.pcap"
-    tcpdump -i eth0 -w $file_name &
+    tshark -i eth0 -w $file_name &
     cmd=${line//@/$i}
     sleep 1
     $cmd
@@ -14,4 +14,5 @@ while [[ i=$((i+1)) -lt 101 ]]; do
     it=$((it+1))
   done <./finger_printing/run_commands.txt
   echo "next $((i+1))"
+  break
 done
