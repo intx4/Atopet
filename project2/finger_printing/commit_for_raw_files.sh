5bfd0b88ec2f07e02e3622e2853c383033fb5c24
elvric <elvric.gtombert@gmail.com>
2021-06-04 17:51:24 +0200

    Cleaned up repo to submit part_3

--- project2/finger_printing/commit_for_raw_files.sh
+++
@@ -1,2 +0,0 @@
-ls ./filtered | xargs sh -c 'for arg do git reset filtered/$arg/.; done'
-ls ./filtered | xargs sh -c 'for arg do git add filtered/$arg/.; git commit -m "added filtered $arg";git push origin part3; done'
--- project2/generate_traces.sh
+++ project2/generate_traces.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-i=2
+i=0
 while [[ i=$((i+1)) -lt 101 ]]; do
   # Change this to change the ouptut file index
-  it=30
+  it=1
   while read -r line; do
     file_name="./finger_printing/raw/cell_$i/network_capture_round_$it.pcap"
     tshark -i eth0 -w $file_name &
